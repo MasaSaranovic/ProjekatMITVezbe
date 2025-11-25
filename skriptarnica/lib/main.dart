af93ff3404bd527c3b9e0995deeb993b63596a0f
MasaSaranovic <masasaranovic@gmail.com>
2025-11-25 20:22:04 +0100

    Vezbe 3

---
+++ skriptarnica/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:skriptarnica/providers/theme_provider.dart';
+import 'package:skriptarnica/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) {
+          return ThemeProvider();
+        }),
+      ],
+      child: Consumer(builder: (context, themeProvider, child) {
+        return MaterialApp(
+          title: 'FTN Skriptarnica',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: const HomeScreen(),
+        );
+      }),
+    );
+  }
+}
