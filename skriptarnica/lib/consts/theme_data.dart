8f59d98aae3266563768cf95de53ca03001295d1
MasaSaranovic <masasaranovic@gmail.com>
2025-12-09 20:13:01 +0100

    Vezbe 5

--- skriptarnica/lib/consts/theme_data.dart
+++ skriptarnica/lib/consts/theme_data.dart
@@ -12,6 +12,16 @@ class Styles {
           : AppColors.lightScaffoldColor,
       cardColor: isDarkTheme ? Colors.grey[800] : AppColors.lightCardColor,
       brightness: isDarkTheme ? Brightness.dark : Brightness.light,
+      appBarTheme: AppBarTheme(
+          backgroundColor: isDarkTheme
+              ? AppColors.darkScaffoldColor
+              : AppColors.lightScaffoldColor,
+          elevation: 0,
+          titleTextStyle: TextStyle(
+            color: isDarkTheme ? Colors.white : Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          )),
     );
   }
 }
